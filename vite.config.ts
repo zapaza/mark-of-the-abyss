51e0b88151b25cc70687aeec8017a577a678259b
stanislavcumakov <3apa3a.iickov@gmail.com>
2026-03-24 21:32:07 +0300

    настройка vite b ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,29 +3,29 @@ import eslint from 'vite-plugin-eslint2';
 
 
 export default defineConfig(({mode}) => {
-    const isDev = mode !== 'production';
+  const isDev = mode !== 'production';
 
-    return {
-        plugins: [
-            eslint()
-        ],
-        resolve: {
-            // 3. Добавьте эту опцию
-            tsconfigPaths: true,
-        },
-        build: {
-            target: 'esnext',
-            minify: 'terser',
-            sourcemap: isDev,
-            assetsInlineLimit: 4096,
-        },
-        server: {
-            port: 3000,
-            open: true,
-            host: true,
-        },
-        define: {
-            __IS_DEBUG__: isDev,
-        },
-    }
+  return {
+    plugins: [
+      eslint(),
+    ],
+    resolve: {
+      // 3. Добавьте эту опцию
+      tsconfigPaths: true,
+    },
+    build: {
+      target: 'esnext',
+      minify: 'terser',
+      sourcemap: isDev,
+      assetsInlineLimit: 4096,
+    },
+    server: {
+      port: 3000,
+      open: true,
+      host: true,
+    },
+    define: {
+      __IS_DEBUG__: isDev,
+    },
+  };
 });
